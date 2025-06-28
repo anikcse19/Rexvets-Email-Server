ce03c8ec3b90cc7376457567bd3b69db356b72ea
Anikcse19 <anikdebcse@gmail.com>
2025-06-28 18:15:36 +0600

    updated

--- server.js
+++ server.js
@@ -194,7 +194,10 @@ export async function generateInvoiceFromHTML(htmlContent, transactionID) {
   // Ensure folder exists
   fs.mkdirSync(path.dirname(filePath), { recursive: true });
 
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
 
   await page.setContent(htmlContent, {
